9225a1993c00c7a8934b5b0a486264f24652aac0
karenlstengel <karenlstengel@gmail.com>
2025-12-04 08:21:03 -0700

    updated derecho build

--- gpu_e3sm_short_test.sh
+++ gpu_e3sm_short_test.sh
@@ -17,11 +17,12 @@ MYCOMPILER=gnugpu
 QUEUE_NAME=main
 
 # CASE_NAME="${COMPSET}.${RESOLUTION}.${MACH}.${MYCOMPILER}.${DYCORE}"
-CASE_NAME="test_10_eamxx_gpu"
+CASE_NAME="test_2_eamxx_gpu"
 CASE_ROOT="$scratch/e3sm_test/${CASE_NAME}"
 CASE_SCRIPTS_DIR=${CASE_ROOT}/case
 CASE_BUILD_DIR=${CASE_ROOT}/build
 CASE_RUN_DIR=${CASE_ROOT}/run
+CASE_ARCHIVE_DIR=${CASE_ROOT}/archive
 export NETCDF_PATH=$NETCDF
 
 ####################################################################
@@ -33,7 +34,7 @@ cd $CCSMROOT/cime/scripts
 
 ./create_newcase --case ${CASE_NAME} --output-root ${CASE_ROOT} --script-root ${CASE_SCRIPTS_DIR} \
                --handle-preexisting-dirs u --compset ${COMPSET} --res ${RESOLUTION} --machine ${MACH} \
-               --compiler ${MYCOMPILER} --project NTDD0004 --walltime "00:59:00"
+               --compiler ${MYCOMPILER} --project NTDD0004 --walltime "00:59:00" --verbose
 
 ####################################################################
 # Configure & Compile
@@ -42,6 +43,8 @@ cd $CASE_SCRIPTS_DIR
 
 ./xmlchange EXEROOT=${CASE_BUILD_DIR}
 ./xmlchange RUNDIR=${CASE_RUN_DIR}
+
+./xmlchange DEBUG=TRUE
 
 ./xmlchange NTASKS=4
 ./xmlchange NTHRDS=1
@@ -53,14 +56,14 @@ cd $CASE_SCRIPTS_DIR
 ./xmlchange OVERSUBSCRIBE_GPU=FALSE
 ./xmlchange ROOTPE='0'
 ./xmlchange DOUT_S=false
+# ./xmlchange DOUT_S_ROOT=${CASE_ARCHIVE_DIR}
 
 ./case.setup
 
 ./xmlchange CAM_TARGET=$DYCORE
 ./xmlchange GMAKE_J='32'
    
-./case.preview
-./case.build
+./case.build #--clean atm # note that you need to have built this at least once successfully before using this flag
 
 #####################################################################
 # Run E3SM
@@ -77,10 +80,9 @@ else
 fi
 ./xmlchange RESUBMIT='0'
 ./xmlchange CONTINUE_RUN='FALSE'
-./xmlchange STOP_N='10',STOP_OPTION='ndays'
-./xmlchange JOB_WALLCLOCK_TIME='00:59:00'
+./xmlchange STOP_N='2',STOP_OPTION='ndays'
+./xmlchange JOB_WALLCLOCK_TIME='00:30:00'
 ./xmlchange JOB_QUEUE=$QUEUE_NAME
-./xmlchange 
 ./xmlchange BUDGETS=TRUE
 
 if [[ $DYCORE == "theta-l_kokkos" ]]; then

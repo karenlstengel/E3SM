5e2c1419da56e04181ac33c578cf4c0c2fc1f9b0
karenlstengel <karenlstengel@gmail.com>
2026-04-27 09:12:29 -0600

    IC from stormspeed setup

--- e3sm_submit_compile_job.sh
+++ e3sm_submit_compile_job.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#PBS -N eamxx_compile
+#PBS -N IC_eamxx
 #PBS -A NTDD0004
 #PBS -j oe
 #PBS -k eod
 #PBS -q develop
 #PBS -l walltime=00:30:00
-#PBS -l select=1:ncpus=64:mem=480GB:ngpus=4:gpu_type=a100
+#PBS -l select=1:ncpus=128:mem=200GB
 #PBS -M kstengel@ucar.edu
 #PBS -m e
 
 # -l select=1:ncpus=128:mem=200GB
 # ./e3sm_kessler_short_cpu.sh
-# ./eamxx_kessler_analyticIC_cpu.sh
+./eamxx_kessler_analyticIC_cpu.sh
 
 
 # -l select=1:ncpus=64:mem=480GB:ngpus=4:gpu_type=a100
-./e3sm_kessler_short_gpu.sh
+# ./e3sm_kessler_short_gpu.sh
